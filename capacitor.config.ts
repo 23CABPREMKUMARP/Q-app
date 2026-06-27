eabc89a4c89a3af4223d057d2a3a68f35385d57c
PREM KUMAR PERUMAL <139529932+23CABPREMKUMARP@users.noreply.github.com>
2026-06-27 22:30:01 +0530

    Fix: Disabled Capacitor splash screen for faster load times

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -22,7 +22,13 @@ const config: CapacitorConfig = {
   android: {
     allowMixedContent: false
   },
-  overrideUserAgent: "Mozilla/5.0 (Linux; Android 13; SM-G991U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36"
+  overrideUserAgent: "Mozilla/5.0 (Linux; Android 13; SM-G991U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36",
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      showSpinner: false
+    }
+  }
 };
 
 export default config;
